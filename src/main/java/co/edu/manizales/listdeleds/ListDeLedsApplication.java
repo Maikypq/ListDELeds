7f392e86bf18e81e5b8109d40e6b9178dae9a4dc
Maikypq <126524781+Maikypq@users.noreply.github.com>
2023-05-08 17:29:34 -0500

    Initial commit

---
+++ src/main/java/co/edu/manizales/listdeleds/ListDeLedsApplication.java
@@ -0,0 +1,13 @@
+package co.edu.manizales.listdeleds;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ListDeLedsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ListDeLedsApplication.class, args);
+    }
+
+}
